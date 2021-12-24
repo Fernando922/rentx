f034e82abb80ee73891eb258d68169eef55e74c6
Fernando de Paula <dipaula018@gmail.com>
2021-12-24 20:52:31 -0300

    feat: expo fonts

--- App.tsx
+++ App.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
+// import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    // return <AppLoading />
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontFamily: "Inter_500Medium", fontSize: 40 }}>
+        Inter Black
+      </Text>
+      <Text style={{ fontSize: 40 }}>Inter Black</Text>
+      <Text style={{ fontFamily: "Archivo_600SemiBold", fontSize: 40 }}>
+        Inter Black
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
